979487ebbaffc017e654711731191a756c614cde
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2020-04-28 02:16:28 +0300

    Add array.merge() function

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Array library of the Chaos language.
+Array library of the Chaos language. You can install this spell with:
+
+```bash
+occultist install array
+```
+
+and import it with:
+
+```chaos
+import array
+```
+
+## Array operations
+
+### list array.merge(list l1, list l2)
+
+Merges list `l1` and list `l2` into a new list, in that order.
+
+```chaos
+kaos> list a = [1, 2, 3]
+kaos> list b = [4, 5, 6]
+kaos> array.merge(a, b)
+[1, 2, 3, 4, 5, 6]
+```
--- array.c
+++ array.c
@@ -1,19 +1,39 @@
 #include "Chaos.h"
 
-char *hello_params_name[] = {};
-unsigned hello_params_type[] = {};
-unsigned short hello_params_length = 0;
-int KAOS_EXPORT Kaos_hello()
+
+// Array operations
+
+// list array.merge(list l1, list l2)
+
+char *merge_params_name[] = {
+    "l1",
+    "l2"
+};
+unsigned merge_params_type[] = {
+    K_LIST,
+    K_LIST
+};
+unsigned short merge_params_length = (unsigned short) sizeof(merge_params_type) / sizeof(unsigned);
+int KAOS_EXPORT Kaos_merge()
 {
-    fprintf(stdout, "Hello from the array!\n");
-    fflush(stdout);
+    kaos.startBuildingList();
+    unsigned long length_i = kaos.getListLength(merge_params_name[0]);
+    for (unsigned long i = 0; i < length_i; i++) {
+        kaos.copyListElement(merge_params_name[0], i);
+    }
+    unsigned long length_j = kaos.getListLength(merge_params_name[1]);
+    for (unsigned long j = 0; j < length_j; j++) {
+        kaos.copyListElement(merge_params_name[1], j);
+    }
+
+    kaos.returnList(K_ANY);
     return 0;
 }
 
 int KAOS_EXPORT KaosRegister(struct Kaos _kaos)
 {
     kaos = _kaos;
-    kaos.defineFunction("hello", K_VOID, hello_params_name, hello_params_type, hello_params_length);
+    kaos.defineFunction("merge", K_LIST, merge_params_name, merge_params_type, merge_params_length);
 
     return 0;
 }
